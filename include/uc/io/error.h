ae545585001653a31745badecdbdd04ae31c3347
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-03-04 21:29:25 +0200

    .

---
+++ include/uc/io/error.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <exception>
+#include <uc/os/windows/com_error.h>
+
+namespace uc
+{
+    namespace io
+    {
+        using base_exception = std::exception;
+        using com_base_exception = uc::os::windows::com_exception;
+
+        class exception : public base_exception
+        {
+            using base = base_exception;
+
+        public:
+            exception(const char* c) : base(c)
+            {
+
+            }
+        };
+
+        class com_exception : public com_base_exception
+        {
+            using base = com_base_exception;
+
+        public:
+
+            com_exception(HRESULT hr) : base(hr)
+            {
+
+            }
+        };
+
+        class out_of_memory_exception : public exception
+        {
+            using base = exception;
+
+        public:
+
+            out_of_memory_exception() : base("out of memory")
+            {
+
+            }
+        };
+
+        inline void throw_if_failed(HRESULT hr)
+        {
+            if (FAILED(hr))
+            {
+                throw com_exception(hr);
+            }
+        }
+
+        inline void raise_out_of_memory_exception()
+        {
+            throw out_of_memory_exception();
+        }
+
+    }
+}
---
+++ src/uc/io/platforms/pc/native_mouse.cpp
@@ -0,0 +1,3 @@
+#include "pch.h"
+
+#include <uc/io/platforms/pc/native_mouse.h>
